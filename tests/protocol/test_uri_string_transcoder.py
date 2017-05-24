4ccec59a0eb14197979d3e59ccf52b05d11e121c
Pankaj Kumar Singh <pankaj28843@gmail.com>
2017-05-24 17:13:35 +0200

    Close #23: Add support for decoding Message field and repeated field using URIStringDictMessageTranscoder

--- tests/protocol/test_uri_string_transcoder.py
+++ tests/protocol/test_uri_string_transcoder.py
@@ -1,6 +1,8 @@
+from typing import List
 from unittest import TestCase
 
 from venom import Message
+from venom.exceptions import ValidationError
 from venom.protocol import URIStringProtocol, URIStringDictMessageTranscoder
 
 
@@ -13,3 +15,29 @@ class URIStringProtocolTestCase(TestCase):
         self.assertEqual(transcoder.encode(Pet(1)), {'petId': '1'})
         self.assertEqual(transcoder.decode({'petId': '42'}), Pet(42))
         self.assertEqual(transcoder.decode({}), Pet())
+
+    def test_decode_message(self):
+        class Foo(Message):
+            name: str
+
+        class Pet(Message):
+            pet_id: int
+            foo: Foo
+
+        protocol = URIStringDictMessageTranscoder(URIStringProtocol, Pet)
+        self.assertEqual(protocol.decode({'petId': '42', 'foo': '{"name": "Bar"}'}), Pet(42, Foo("Bar")))
+
+        with self.assertRaises(ValidationError):
+            protocol.decode({'petId': '42', 'foo': '{123'})
+
+
+    def test_decode_repeat_message(self):
+        class Foo(Message):
+            name: str
+
+        class Pet(Message):
+            pet_id: int
+            foo: List[Foo]
+
+        protocol = URIStringDictMessageTranscoder(URIStringProtocol, Pet)
+        self.assertEqual(protocol.encode(Pet(42, [Foo("Bar")])), {'petId': '42', 'foo': ['{"name":"Bar"}']})
