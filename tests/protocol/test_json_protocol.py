0f4a6cd986d1ec8c9432c3548739a655480ffc6f
Ali Kaafarani <ali@kvikshaug.no>
2018-01-24 20:14:35 +0100

    test: encode/decode message with zero numeric value

--- tests/protocol/test_json_protocol.py
+++ tests/protocol/test_json_protocol.py
@@ -31,6 +31,14 @@ class JSONProtocolTestCase(TestCase):
         self.assertEqual(e.exception.description, "'bad' is not of type 'object'")
         self.assertEqual(e.exception.path, [])
 
+    def test_encode_zero_numeric(self):
+        class Pet(Message):
+            cuteness: float
+
+        protocol = JSONProtocol(Pet)
+        self.assertEqual(protocol.encode(Pet(0.0)), {'cuteness': 0.0})
+        self.assertEqual(protocol.decode({'cuteness': 0.0}), Pet(0.0))
+
     def test_encode_with_field_mask(self):
         class Pet(Message):
             sound: str
